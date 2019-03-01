9e6935e51218c9c1b11cdc89f12b5506b6a47e55
Mohamed Khaled Yousef <mkhy88519@gmail.com>
2019-03-01 22:43:59 +0200

    Add primitive types

---
+++ JS/ES6/Primitive and Reference Types/Primitive.js
@@ -0,0 +1,15 @@
+const number1 = 5;
+const number2 = number1;
+
+console.log(number2);
+
+let myName = "My name is " +
+                 "Mohamed " +
+                 ", Front End Developer.";
+console.log(myName);
+
+var x1= 0, x2=-50, y1=1, y2=50;
+console.log(Boolean(x1));       // returns false
+console.log(Boolean(x2));       // returns true
+console.log(Boolean(y1));       // returns true
+console.log(Boolean(y2));       // returns true
