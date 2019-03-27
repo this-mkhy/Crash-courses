d72d764c40c83819eec3e0723a0de47b46fc56c2
Mohamed Khaled Yousef <mkhy88519@gmail.com>
2019-03-27 14:56:40 +0200

    Update reference types

--- JS/ES6/Primitive and Reference Types/Reference.js
+++ JS/ES6/Primitive and Reference Types/Reference.js
@@ -26,3 +26,32 @@ console.log(person2);
 
 person1.name = 'Khaled';
 console.log(person2);
+
+//-----------------------
+const myInfo = { 
+  name: 'Mohamed',
+  age: 13
+}  
+
+
+const copy = myInfo;
+copy.age = 50;
+copy.name = 'Khaled';
+
+console.log(myInfo);
+console.log(copy);
+
+//----------------------
+
+const person1 = {
+  name : 'Mohamed'
+};
+
+const person2 = {
+  ...person1
+}
+
+console.log(person2);
+
+person1.name = 'Khaled';
+console.log(person2);
