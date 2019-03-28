db5571952c78f66f3de5ac767736810042202fc4
Mohamed Khaled Yousef <mkhy88519@gmail.com>
2019-03-29 00:04:57 +0200

    Update array functions

--- JS/ES6/ArrayFunctions.js
+++ JS/ES6/ArrayFunctions.js
@@ -15,3 +15,22 @@ console.log(names);
 names.splice(2, 3, "Computer", "Science");
 console.log(names);
 
+//-------------------
+
+const numbers = [1,2,3];
+const doubleNumbers = numbers.map((num) => {
+  return num*2;
+});
+   
+console.log(numbers);		    //[1, 2, 3]
+console.log(doubleNumbers);	//[2, 4, 6]
+
+//-------------------
+
+const myName = ["Mohamed", "Khaled"];
+const myInfo1 = ["Computer", "Science", "Student"];
+const myInfo2 = ["Front End Developer"];
+
+const concatTwoArray = (arr1,arr2,arr3) => arr1.concat(arr2,arr3);
+console.log(concatTwoArray(myName,myInfo1,myInfo2));
+
